d16220381717f69aeeaaa9762b4e6ab5d2104215
Sayara Kaylaine <sayara12kaylaine@gmail.com>
2024-08-07 21:33:12 -0400

    migration funcionarios/servicos

---
+++ database/migrations/1723080709697_create_funcionarios_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'funcionarios'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('nome', 45).notNullable()
+      table.string('funcao', 45).notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
