ba2a571e5d41b3e03180139b2c7093549f6359d2
Peterson de Almeida Oenning <oenningpeterson9@gmail.com>
2024-08-07 22:07:30 -0400

    update routes/cadastro serviço/ serviço controller

--- start/routes.ts
+++ start/routes.ts
@@ -1,40 +1,31 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 
-//serviço
+// Renders
 router.on('/').render('components/layout/index')
 router.on('/cadastroservico').render('components/layout/cadastro_servico')
 router.on('/consultaservico').render('components/layout/consulta_servico')
 router.on('/relatorioservico').render('components/layout/relatorio_servico')
 
-// horário
-
+// Horário
 router.on('/cadastrohorario').render('components/layout/cadastro_horario')
 router.on('/consultahorario').render('components/layout/consulta_horario')
 router.on('/relatoriohorario').render('components/layout/relatorio_horario')
 
-// funcionário
-
+// Funcionário
 router.on('/cadastrofuncionario').render('components/layout/cadastro_funcionario')
 router.on('/consultafuncionario').render('components/layout/consulta_funcionario')
 router.on('/relatoriofuncionario').render('components/layout/relatorio_funcionario')
 
-// ordem de serviço
-
+// Ordem de Serviço
 router.on('/cadastroordemdeservico').render('components/layout/cadastro_ordemdeservico')
 router.on('/consultaordemdeservico').render('components/layout/consulta_ordemdeservico')
 router.on('/relatorioordemdeservico').render('components/layout/relatorio_ordemdeservico')
 
-// cliente
-
+// Cliente
 router.on('/cadastrocliente').render('components/layout/cadastro_cliente')
 router.on('/consultacliente').render('components/layout/consulta_cliente')
 router.on('/relatoriocliente').render('components/layout/relatorio_cliente')
+
+// API
+router.post('/api/servicos', 'ServicosController.store')
+router.get('/api/servicos', 'ServicosController.index')
